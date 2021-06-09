ed18f0535c1cfee3eb23df8c0be029e230d08548
sseriming <84060181+sseriming@users.noreply.github.com>
2021-06-09 12:15:15 +0900

    Update project9.java

--- src/javaproject/project9.java
+++ src/javaproject/project9.java
@@ -1,4 +1,5 @@
 package javaproject;
+
 import java.util.Scanner;
 
 public class project9 {
@@ -8,23 +9,17 @@ public class project9 {
 		int tmp;
 		Scanner sc = new Scanner(System.in);
 		input = sc.nextLine();
-		for(int i=0;i<input.length();i++)
-		{
-			tmp=(int)input.charAt(i);
-			if(65<=tmp && tmp<=90)
-			{
-				output +=(char)(tmp+3);
-			}
-			else if(97<=tmp&&tmp<=122) {
-				output +=(char)(tmp+3);
-			}
-			else {
-				output +=(char)(tmp);
+		for (int i = 0; i < input.length(); i++) {
+			tmp = (int) input.charAt(i);
+			if (65 <= tmp && tmp <= 90) {
+				output += (char) (tmp + 3);
+			} else if (97 <= tmp && tmp <= 122) {
+				output += (char) (tmp + 3);
+			} else {
+				output += (char) (tmp);
 			}
 		}
 		System.out.println(output);
 		sc.close();
 	}
-	}
-
-
+}
