417ff46c4f67139688d3aa1336a04146c8670999
sseriming <84060181+sseriming@users.noreply.github.com>
2021-06-09 12:14:24 +0900

    Update project6.java

--- src/javaproject/project6.java
+++ src/javaproject/project6.java
@@ -1,20 +1,18 @@
 package javaproject;
+
 import java.util.Scanner;
 
 public class project6 {
 	public static void main(String[] args) {
 		int[] student;
-		int sum=0;
-		Scanner sc=new Scanner(System.in);
+		int sum = 0;
+		Scanner sc = new Scanner(System.in);
 		student = new int[10];
-		for(int i=0;i<10;i++)
-		{
-			student[i]=sc.nextInt();
-			sum +=student[i];
-			
+		for (int i = 0; i < 10; i++) {
+			student[i] = sc.nextInt();
+			sum += student[i];
 		}
-
-		System.out.println("ÇÕ°è : "+sum+"\nÆò±Õ : "+(sum/10));
+		System.out.println("Ã‡Ã•Â°Ã¨ : " + sum + "\nÃ†Ã²Â±Ã• : " + (sum / 10));
 		sc.close();
 	}
 }
