6f463fbb3ae2bfff0f7ecb8a5219d306c463ef08
sseriming <84060181+sseriming@users.noreply.github.com>
2021-06-09 12:14:42 +0900

    Update project7.java

--- src/javaproject/project7.java
+++ src/javaproject/project7.java
@@ -1,21 +1,16 @@
 package javaproject;
 
-
 public class project7 {
 	public static void main(String[] args) {
-	double[][] student = {{90,80,80},
-			{85,85,95},
-			{95,70,75},
-			{80,80,90},
-			{90,75,80}};
-	double[] sum = new double[5];
-	for(int i=0;i<5;i++) {
-		for(int j=0;j<3;j++) {
-			sum[i] += student[i][j];
+		double[][] student = { { 90, 80, 80 }, { 85, 85, 95 }, { 95, 70, 75 }, { 80, 80, 90 }, { 90, 75, 80 } };
+		double[] sum = new double[5];
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 3; j++) {
+				sum[i] += student[i][j];
+			}
+			double av;
+			av = sum[i] / 3;
+			System.out.println("ID=" + i + " : " + "Ã‡Ã•Â°Ã¨ = " + sum[i] + " Ã†Ã²Â±Ã• = " + (Math.round(av * 100) / 100.0));
 		}
-		double av;
-		av=sum[i]/3;
-		System.out.println("ID="+i+" : "+"ÇÕ°è = "+sum[i]+" Æò±Õ = "+(Math.round(av*100)/100.0));
-	}
 	}
 }
