fa007373c7aef81ed2caec6737fcf1186fc2a817
sseriming <84060181+sseriming@users.noreply.github.com>
2021-06-09 12:27:40 +0900

    Update project5.java

--- src/javaproject/project5.java
+++ src/javaproject/project5.java
@@ -8,15 +8,15 @@ public class project5 {
 		int n;
 		n = sc.nextInt();
 		if (n >= 90)
-			System.out.println("ÇÐÁ¡ : A");
+			System.out.println("학점 : A");
 		else if (80 <= n && n < 90)
-			System.out.println("ÇÐÁ¡ : B");
+			System.out.println("학점 : B");
 		else if (70 <= n && n < 80)
-			System.out.println("ÇÐÁ¡ : C");
+			System.out.println("학점 : C");
 		else if (60 <= n && n < 70)
-			System.out.println("ÇÐÁ¡ : D");
+			System.out.println("학점 : D");
 		else
-			System.out.println("ÇÐÁ¡ : F");
+			System.out.println("학점 : F");
 		sc.close();
 	}
 
