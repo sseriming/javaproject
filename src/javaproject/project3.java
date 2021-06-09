4f845c4eb16249df5af25e88a8127a9eca5f5f0a
sseriming <84060181+sseriming@users.noreply.github.com>
2021-06-09 12:13:01 +0900

    Update project3.java

--- src/javaproject/project3.java
+++ src/javaproject/project3.java
@@ -2,11 +2,10 @@ package javaproject;
 
 public class project3 {
 	public static void main(String[] args) {
-		for(int i=1;i<10;i++)
-		{
-			System.out.println("** "+i+"´Ü **");
-			for(int j=1;j<10;j++) {
-				System.out.println(i+"*"+j+"="+(i*j));
+		for (int i = 1; i < 10; i++) {
+			System.out.println("** " + i + "Â´Ãœ **");
+			for (int j = 1; j < 10; j++) {
+				System.out.println(i + "*" + j + "=" + (i * j));
 			}
 			System.out.println();
 		}
